07227af2d95114c8be419f57820a0d9679ab12de
MarshalJoe <joseph.charles.marshall@gmail.com>
2015-03-14 13:48:50 -0500

    Adds index.html

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var app = require('express')();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+	res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on port 3000');
 });
